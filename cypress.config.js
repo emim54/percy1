222fb2386a5b0b7030b94fbaa998e55db44a75ea
emim54 <156033266+emim54@users.noreply.github.com>
2024-01-23 16:56:27 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    supportFile: false // Setzen Sie supportFile auf false, wenn Sie keine Support-Datei benötigen
   },
 });
